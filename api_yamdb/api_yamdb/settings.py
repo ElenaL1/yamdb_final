10233fcd8c87d5bd7c4dd7a237a064b7542269c6
Elena Lambert <elena_lambert@yahoo.com>
2023-05-22 16:03:36 +0300

    4 commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -90,10 +90,7 @@ DATABASES = {
 #     }
 # }
 
-
-
 # Password validation
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
